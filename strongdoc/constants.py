44a24b3f2fa67c8a9d6426eb2d166638877c548c
Kathy Lam <kathy@overnest.com>
2020-03-03 16:01:31 -0800

    Update to use StrongDoc domain name

--- strongdoc/constants.py
+++ strongdoc/constants.py
@@ -3,9 +3,9 @@ from os import path
 """
 Location of StrongDoc Service
 """
-HOST = 'localhost'  # UPDATE TO USE api.strongsalt.com
+HOST = 'api.strongsalt.com'
 PORT = 9090
-CERT = path.join(path.dirname(__file__), "data", "certs", "grpc.root.localhost.pem")  # UPDATE TO USE grpc.root.pem`
+CERT = path.join(path.dirname(__file__), "data", "certs", "grpc.root.pem")
 
 """
 Timeout value for the gRPC connection
